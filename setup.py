fb58eee07f513da476b76626fe5f23868a97ddc1
tngodup <tenzin.ngodup@intel.com>
2018-02-14 19:43:22 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='elk-py',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['_init_.py'],                  # The name of your scipt, and also the command you'll be using for calling it
+    description='The funniest joke in the world',
+    url='https://github.com/tenzinngodup/elk-py.git',
+    author='Tenzin Ngodup',
+    author_email='tenngo@yahoo.com',
+    license='MIT',
+    zip_safe=False
+)
