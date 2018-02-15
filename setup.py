67e3b1f5d800ec96dd90fa4f28475a8354f86f78
tngodup <tenzin.ngodup@intel.com>
2018-02-14 20:28:18 -0800

    third change

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='elk-py',    # This is the name of your PyPI-package.
-    version='0.1',                          # Update the version number for new releases
-    scripts=['_init_.py'],                  # The name of your scipt, and also the command you'll be using for calling it
+    version='0.2',                          # Update the version number for new releases
+#    scripts=['_init_.py'],                  # The name of your scipt, and also the command you'll be using for calling it
     description='The funniest joke in the world',
     url='https://github.com/tenzinngodup/elk-py',
     author='Tenzin Ngodup',
